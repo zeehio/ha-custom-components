3179f9498f0d59f4dfa980e33365501a315fedab
Sergio Oller <sergioller@gmail.com>
2025-06-23 04:37:21 +0200

    Remove, not needed

--- local_calendar/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the Local Calendar integration."""
-
-DOMAIN = "local_calendar"
-
-CONF_CALENDAR_NAME = "calendar_name"
-CONF_CALENDAR_URL = "calendar_url"
-CONF_STORAGE_KEY = "storage_key"
-CONF_SYNC_INTERVAL = "update_interval"
